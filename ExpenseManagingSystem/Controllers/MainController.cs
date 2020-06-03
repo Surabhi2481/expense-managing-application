58848e1467288ca8429a171c42a0bd5ad2ad3e92
Surabhi2481 <surabhibbsr2481@gmail.com>
2020-06-03 23:27:29 +0530

    added login and verified credentials

--- ExpenseManagingSystem/Controllers/MainController.cs
+++ ExpenseManagingSystem/Controllers/MainController.cs
@@ -26,7 +26,7 @@ namespace ExpenseManagingSystem.Controllers
         public ActionResult logout()
         {
             Session.Abandon();
-            return RedirectToAction("Register");
+            return RedirectToAction("Home");
         }
 
         public ActionResult Register()
@@ -74,5 +74,45 @@ namespace ExpenseManagingSystem.Controllers
         {
             return View(db.Expenses.ToList());
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(mainClass real)
+        {
+
+            using (DataContext db = new DataContext())
+            {
+                mainClass obj = db.mainClasss.FirstOrDefault(user => user.userId == real.userId && user.password == real.password);
+                if (obj == null)
+                    return View("vendorRejected");
+
+
+                if (obj != null)
+                {
+                    Session["userid"] = obj.userId;
+                    /*if (obj.categoryId == categ.user)
+                        return RedirectToAction("userProductList");
+                    else if (obj.categoryId == categ.vendor)
+                        return RedirectToAction("vendorLoginCheck");
+                    else
+                        return RedirectToAction("admin");*/
+                    return RedirectToAction("expenseList"); ;
+                }
+            }
+            return View(real);
+        }
+
+        public ActionResult userExpenseList()
+        {
+            var a = (from d in db.Expenses select d).ToList();
+            return View(a);
+        }
+
+
     }
 }
